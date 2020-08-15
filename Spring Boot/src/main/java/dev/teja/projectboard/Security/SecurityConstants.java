a8406b2958785c0ab29ac448418cd6cb48ccaed5
saiteja <kondasaitej@protonmail.com>
2020-08-15 17:36:11 +0530

    Token Expiration time is set to 10 days

--- Spring Boot/src/main/java/dev/teja/projectboard/Security/SecurityConstants.java
+++ Spring Boot/src/main/java/dev/teja/projectboard/Security/SecurityConstants.java
@@ -9,6 +9,6 @@ public class SecurityConstants {
     public static final String HEADER_STRING = "Authorization";
     public static final String SWAGGER_UI = "/swagger-ui/**";
     public static final String SWAGGER_UI_V3 ="/v3/api-docs/**";
-    public static final long EXPIRATION_TIME =  30_000; //10 DAYS 864_000_000;
+    public static final long EXPIRATION_TIME =  864_000_000 ; //30Seconds 30_000;
 }
 

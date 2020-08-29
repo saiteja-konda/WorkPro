dc8e6f81cb9b609f566deeeb4d4a9937882e9a28
saiteja <kondasaitej@protonmail.com>
2020-08-30 01:05:32 +0530

    build

---
+++ React/front-end/build/precache-manifest.4e2bef05a3d135346ce54174603c9178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1c9726aa1c96cc2071480072497714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf500a3aedbf52c97c52",
+    "url": "/static/css/2.cc5e6184.chunk.css"
+  },
+  {
+    "revision": "e47afb9ce32598474dec",
+    "url": "/static/css/main.16e413c6.chunk.css"
+  },
+  {
+    "revision": "bf500a3aedbf52c97c52",
+    "url": "/static/js/2.5639d56e.chunk.js"
+  },
+  {
+    "revision": "2113dd0da358adb153ee5b1332461c70",
+    "url": "/static/js/2.5639d56e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47afb9ce32598474dec",
+    "url": "/static/js/main.5f2c8ef3.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);

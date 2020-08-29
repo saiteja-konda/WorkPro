1c2a60f756c41ff4bedce74655656aedd9bb28bc
saiteja <kondasaitej@protonmail.com>
2020-08-30 02:56:44 +0530

    pivotol cloud foundry

--- React/front-end/src/urlConfig.js
+++ React/front-end/src/urlConfig.js
@@ -1,4 +1,4 @@
-export const PROJECT_API_URL = "http://localhost:8080/api/project/";
-export const PROJECT_BACKLOG_API_URL = "http://localhost:8080/api/backlog/";
-export const USER_LOGIN_API_URL = "http://localhost:8080/api/users/login";
-export const USER_REGISTER_API_URL = "http://localhost:8080/api/users/register";
+export const PROJECT_API_URL = "https://projectboard.cfapps.io/api/project/";
+export const PROJECT_BACKLOG_API_URL = "https://projectboard.cfapps.io/api/backlog/";
+export const USER_LOGIN_API_URL = "https://projectboard.cfapps.io/api/users/login";
+export const USER_REGISTER_API_URL = "https://projectboard.cfapps.io/api/users/register";

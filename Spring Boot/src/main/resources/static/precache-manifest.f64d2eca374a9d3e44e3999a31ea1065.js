75d3cde7c126d69fb453c4258214591bec74e9a9
saiteja <kondasaitej@protonmail.com>
2020-09-26 00:34:42 +0530

    clean

---
+++ Spring Boot/src/main/resources/static/precache-manifest.f64d2eca374a9d3e44e3999a31ea1065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68982fd4e4f7af348fcc4b76d2debee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941737a00181bed26e64",
+    "url": "/static/css/2.cc5e6184.chunk.css"
+  },
+  {
+    "revision": "e68e0d3c62ab41aa04f4",
+    "url": "/static/css/main.16e413c6.chunk.css"
+  },
+  {
+    "revision": "941737a00181bed26e64",
+    "url": "/static/js/2.f1b27d46.chunk.js"
+  },
+  {
+    "revision": "2113dd0da358adb153ee5b1332461c70",
+    "url": "/static/js/2.f1b27d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68e0d3c62ab41aa04f4",
+    "url": "/static/js/main.a62da629.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);

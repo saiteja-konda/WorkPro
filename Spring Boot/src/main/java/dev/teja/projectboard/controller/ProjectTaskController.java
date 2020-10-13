8aa0368b7e02adbcd577be4d7d8e3f04f0a2f73f
saiteja konda <55386039+saiteja-konda@users.noreply.github.com>
2020-10-13 16:43:10 +0530

    Update ProjectTaskController.java

--- Spring Boot/src/main/java/dev/teja/projectboard/controller/ProjectTaskController.java
+++ Spring Boot/src/main/java/dev/teja/projectboard/controller/ProjectTaskController.java
@@ -11,7 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/board")
 public class ProjectTaskController {
